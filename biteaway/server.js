c1ab4f736657057bd14eff1f4aba3875c9daac29
Joshna <joshna.p2507@gmail.com>
2025-04-06 11:45:01 -0700

    minor correction in findCuisine function

--- biteaway/server.js
+++ biteaway/server.js
@@ -11,6 +11,12 @@ var restaurantRouter = require('./routes/restaurant');
 var userRouter = require('./routes/user');
 var adminRouter = require('./routes/admin');
 var orderRouter = require('./routes/order');
+const User = require("./models/User");
+const Order = require("./models/Order");
+const Restaurant = require("./models/Restaurant");
+const Review = require("./models/Review");
+const Item = require("./models/Item");
+const Cuisine = require("./models/Cuisine");
 
 // const fs = require("fs")
 const app = express();
@@ -46,6 +52,68 @@ app.use('/admin', adminRouter); //joshna's admin pages (order management)
 //this will probably have to be changed later
 async function setup() {
   //adding sample data?
+  const John= await User.create({
+    userID: 101,
+    firstName: "John",
+    lastName: "Doe",
+    countryCode: 1,
+    phoneNumber: 1234567890,
+    userAddress: "123 Baker's Street, Pullman, WA",
+    email: "johndoe@example.com",
+    password: "securepassword"
+  });
+
+  const rev= await Review.create({
+    reviewID: 1,
+    userID: 101,
+    reviewRating: 5,
+    reviewDescription: "Good food, quick service and helpful staff.",
+  });
+
+  const res= await Restaurant.create({
+    restaurantID: 1,
+    restaurantName: "Thai Ginger",
+    restaurantAddress: "256 Forest Avenue, Pullman, WA",
+    //cuisines: "Asian",
+    // reviews
+    restaurantRating: 4.5,
+  });
+
+  const ord= await Order.create({
+    orderID: 1,
+    userID: 101,
+    restaurantID: 1,
+    orderDate: new Date(),
+    orderPrice: 27.98,
+    userAddress: "123 Baker's Street, Pullman, WA",
+    status: "Pending",
+  });
+
+  const item1= await Item.create({
+    itemID: 3,
+    restaurantID: 1,
+    itemName: "Pad Thai",
+    itemPrice: 12.99,
+    itemDescription: "Stir fried rice noddles with egg, vegetables and peanuts ",
+  });
+
+  const item2= await Item.create({
+    itemID: 4,
+    restaurantID: 1,
+    itemName: "Thai Green Curry",
+    itemPrice: 14.99,
+    itemDescription: "Spicy curry with coconut currey, vegetables and your choice of protein",
+  });
+
+  const cuisine1 = await Cuisine.create({
+    cuisineType: "Thai",
+    restaurantID: 1,
+  });
+
+  const cuisine2 = await Cuisine.create({
+    cuisineType: "Asian",
+    restaurantID: 1,
+  });
 }
 
 //sync and setup database
