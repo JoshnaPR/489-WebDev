024ddfc76e34c5267a03619be0432ad9202da4cd
trisha <trisha@teredesai.com>
2025-04-06 22:58:38 -0700

    added user route

--- biteaway/routes/user.js
+++ biteaway/routes/user.js
@@ -5,4 +5,26 @@ router.use("/static/", express.static("static"))
 
 //home, favorites, order history,recently-viewed, settings
 
+router.get("/", async function (req, res) {
+
+    res.render('userHome')
+
+})
+
+router.get("/favorites", async function (req, res) {
+    res.render('userFavorites')
+})
+
+router.get("/order-history", async function (req, res) {
+    res.render('userOrderHistory')
+})
+
+router.get("/recently-viewed", async function (req, res) {
+    res.render('userRecentlyViewed')
+})
+
+router.get("/settings", async function (req, res) {
+    res.render('userSettings')
+})
+
 module.exports = router;
