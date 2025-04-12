bc15a1a564648898097cc6aa3d346760a22ed226
Hannah Garcia <hkgarcia04@gmail.com>
2025-04-12 13:47:51 -0700

    backend for leaving reviews completed

--- biteaway/routes/restaurant.js
+++ biteaway/routes/restaurant.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var controller = require('../controllers/restaurant');
+var reviewController = require('../controllers/review');
 
 router.use("/static/", express.static("static"))
 
@@ -12,5 +13,16 @@ router.get("/:id/menu", controller.getRestaurantMenu);
 
 // reviews page
 router.get("/:id/reviews", controller.getRestaurantReviews);
+
+// reviews - creating/posting
+router.post("/:id/reviews", (req, res) => {
+    console.log("Inside of router.post for reviews")
+    // console.log(req.body)
+    
+    // getting information
+    const { ratingRange, reviewText, userID, restaurantID } = req.body
+
+    reviewController.leaveReview(ratingRange, reviewText, userID, restaurantID, res);
+})
 
 module.exports = router;
