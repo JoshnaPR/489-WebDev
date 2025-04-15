ab49a3888ac9119bc2986af3918fcef070786686
JoshnaP1 <j47815067@gmail.com>
2025-04-15 17:57:45 +0530

    Removed incorrect folders migrated from personal branch

--- backend/database.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize({
-    dialect: 'sqlite',
-    storage: './database/database.sqlite',
-    logging: console.log,
-});
-
-module.exports = sequelize;
